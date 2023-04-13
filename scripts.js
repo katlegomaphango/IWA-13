4b25dbc2f1f2233d8138a3691c80c48e41a28dd1
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-13 12:40:52 +0200

    Added comments to fixed parts

--- scripts.js
+++ scripts.js
@@ -4,21 +4,25 @@ let calculated = '1'
 
 // Only allowed to change below
 
+const logCalc = () => { 
+    // const isString = typeof calculated = 'numerical-string'
+    const isString = typeof calculated === typeof 'numerical-string' 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+    // const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+    const calculatedAsNumber = isString ? parseInt(calculated) : calculated
+
+    // calculated === calculatedAsNumber + 1
+    calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
+const calcUser = () => {
+  logCalc() // logCalc
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
